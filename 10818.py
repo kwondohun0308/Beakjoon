7e5490321b4e58040586387cfa9f855563d69dd2
kwondohun0308 <87187428+kwondohun0308@users.noreply.github.com>
2021-07-14 22:34:39 +0900

    2021-07-14

---
+++ 10818.py
@@ -0,0 +1,4 @@
+a = int(input())
+N = list(map(int,input().split()))
+N=sorted(N)
+print(N[0],N[-1])
---
+++ 10952.py
@@ -0,0 +1,7 @@
+while True:
+    b,c=map(int,input().split())
+    if b==0 and c==0:
+        break
+    else:
+        print(b+c)
+    
